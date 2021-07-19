4a022dccb31c60727f2cd504348ea7fb678f6bd2
degemarps <degemarpereira@gmail.com>
2021-07-18 22:48:22 -0300

    Modificação da propriedade cpf para aplicar o private set

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Degemar"
+
     var cpf: String = "123.123.123-11"
+    private set
 }
 
 fun main() {
     val degemar = Pessoa()
 
+    println(degemar)
     println(degemar.nome)
     println(degemar.cpf)
+
 }
