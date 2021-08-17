6f4a4fd04021ee16bb6f8ead91cd32629a43b221
degemarps <degemarpereira@gmail.com>
2021-08-17 12:04:47 -0300

    update para criar e implementar classes abstratas

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Degemar"
-
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val degemar = Pessoa()
-
-    println(degemar.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
