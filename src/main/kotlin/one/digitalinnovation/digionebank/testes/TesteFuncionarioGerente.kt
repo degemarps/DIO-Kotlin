8adc53673d9fb328bcdfde40c1f12a8359f1c1a4
degemarps <degemarpereira@gmail.com>
2021-08-17 13:22:55 -0300

    update da classe Gerente e o seu teste

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,7 +4,9 @@ import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
 
-    val maria = Gerente("Maria do Carmo", "1234567890", 5000.0)
+    val maria = Gerente("Maria do Carmo", "1234567890", 5000.0, "senha123")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
